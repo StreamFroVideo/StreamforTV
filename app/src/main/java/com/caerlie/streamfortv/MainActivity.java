3eb695b1f7fac958e3a2b5ab51d1255e01d73c17
caerlie <yuanxiaolin.pm@gmail.com>
2015-03-08 16:26:18 +0900

    change comand

--- app/src/main/java/com/caerlie/streamfortv/MainActivity.java
+++ app/src/main/java/com/caerlie/streamfortv/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Over
+        //Over For Android
     }
 
 
